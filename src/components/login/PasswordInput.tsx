37856013b9ccf727fe475f4e888670f8a8c98e78
v154254 <v154254@yandex.ru>
2023-08-16 14:38:18 +0500

    fix: change password input type

--- src/components/login/PasswordInput.tsx
+++ src/components/login/PasswordInput.tsx
@@ -2,7 +2,7 @@ import React, { ComponentPropsWithoutRef } from 'react';
 import classes from './LoginInputs.module.css';
 
 function PasswordInput({ placeholder }: ComponentPropsWithoutRef<'input'>) {
-  return <input type='text' placeholder={placeholder} className={classes.input} />;
+  return <input type='password' placeholder={placeholder} className={classes.input} />;
 }
 
 export default PasswordInput;
