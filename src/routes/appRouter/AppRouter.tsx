a8da12d6c819dcb99ecbe18b1ad5607aa50772a6
Rigelion <cupofbeer123@gmail.com>
2023-08-21 19:43:54 +0300

    feat: implement routing

--- src/routes/appRouter/AppRouter.tsx
+++ src/routes/appRouter/AppRouter.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 import Login from '../../pages/Login';
 import Home from '../../pages/Home';
@@ -9,13 +9,16 @@ import NotFound from '../../pages/NotFound';
 import Profile from '../../pages/Profile';
 
 import ROUTES from '../routes';
+import AuthContext from '../../core/utils/authContext';
 
 function AppRouter() {
+  const { isAuth } = useContext(AuthContext);
+
   return (
     <div>
       <Routes>
         <Route path={ROUTES.HOME} element={<Home />} />
-        <Route path={ROUTES.LOGIN} element={<Login />} />
+        <Route path={ROUTES.LOGIN} element={isAuth ? <Navigate to='/' /> : <Login />} />
         <Route path={ROUTES.CART} element={<Cart />} />
         <Route path={ROUTES.REGISTRATION} element={<Registration />} />
         <Route path={ROUTES.PROFILE} element={<Profile />} />
