92ec9c8fa3ec1b60d6d680cde2ef1a73fa859e9e
v154254 <v154254@yandex.ru>
2023-08-26 06:22:56 +0500

    feat: add functions for getting and updating customers

---
+++ src/core/services/updateCustomerById.ts
@@ -0,0 +1,24 @@
+import { Actions } from '../../types/updatesRequests-types';
+import { apiConstants } from './apiConstants';
+
+export default async function updateUserByID(
+  id: string | null,
+  version: number | null,
+  actions: Actions,
+) {
+  const url = `${apiConstants.apiUrl}/${apiConstants.projectKey}/customers/${id}`;
+  const token = localStorage.getItem('accessToken');
+  const bodyRequest = {
+    version,
+    actions,
+  };
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(bodyRequest),
+  });
+  return res;
+}
