2c4670dcc12e46db2c313b547c53c20b1e79deca
v154254 <v154254@yandex.ru>
2023-08-26 06:23:59 +0500

    feat: now you can update personal info in profile

--- src/components/profile/ProfileAttribute.tsx
+++ src/components/profile/ProfileAttribute.tsx
@@ -1,12 +1,100 @@
-import React from 'react';
+import React, { ComponentPropsWithoutRef, useState } from 'react';
+import { Actions } from '../../types/updatesRequests-types';
+import updateUserByID from '../../core/services/updateCustomerById';
 
-function ProfileAttribute({ headerText }: { headerText: string }) {
+interface ProfileAttributes extends ComponentPropsWithoutRef<'input'> {
+  headerText: string;
+  userID: string | null;
+  userVersion: number | null;
+}
+
+function ProfileAttribute({ headerText, userID, userVersion, ...props }: ProfileAttributes) {
+  const [inputIsDisabled, changeInputDisabled] = useState(true);
+  const [updateIsDisabled, changeUpdateDisabled] = useState(false);
+  const [saveIsDisabled, changeSaveDisabled] = useState(true);
+  const [inputValue, setValue] = useState('');
+  const [emailWarning, toggleEmailWarning] = useState('');
+  function checkEmail(value: string) {
+    if (headerText === 'E-Mail') {
+      toggleEmailWarning('');
+      changeSaveDisabled(false);
+      const validRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+      if (value === '') {
+        toggleEmailWarning('E-mail is required');
+        changeSaveDisabled(true);
+      } else if (value !== value.trim()) {
+        toggleEmailWarning('Please delete trails at the beginning and/or end of e-mail');
+        changeSaveDisabled(true);
+      } else if (!value.match(validRegex)) {
+        toggleEmailWarning(
+          `Incorrect e-mail. E-mail should have a name and domain, for example: 'test@test.com'`,
+        );
+        changeSaveDisabled(true);
+      }
+    }
+  }
+  function enableInput() {
+    changeInputDisabled(false);
+    changeUpdateDisabled(true);
+    changeSaveDisabled(false);
+  }
+  function tryToUpdate() {
+    const actions: Actions = [];
+    switch (headerText) {
+      case 'E-Mail':
+        actions.push({
+          action: 'changeEmail',
+          email: inputValue,
+        });
+        break;
+      case 'First name':
+        actions.push({
+          action: 'setFirstName',
+          firstName: inputValue,
+        });
+        break;
+      case 'Last name':
+        actions.push({
+          action: 'setLastName',
+          lastName: inputValue,
+        });
+        break;
+      case 'Date of birth':
+        actions.push({
+          action: 'setDateOfBirth',
+          dateOfBirth: inputValue,
+        });
+        break;
+      default:
+        break;
+    }
+    updateUserByID(userID, userVersion, actions).then(
+      () => {},
+      () => {},
+    );
+    changeInputDisabled(true);
+    changeUpdateDisabled(false);
+    changeSaveDisabled(true);
+  }
   return (
     <div>
       <header>{headerText}</header>
-      <input />
-      <button type='button'>Change</button>
-      <button type='button'>Save</button>
+      <input
+        defaultValue={props.value}
+        type={props.type}
+        disabled={inputIsDisabled}
+        onChange={(e) => {
+          setValue(e.target.value);
+          checkEmail(e.target.value);
+        }}
+      />
+      <button type='button' onClick={enableInput} disabled={updateIsDisabled}>
+        Update
+      </button>
+      <button type='button' onClick={tryToUpdate} disabled={saveIsDisabled}>
+        Save
+      </button>
+      <p>{emailWarning}</p>
     </div>
   );
 }
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,17 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from '../components/layout/container/Container';
 import ProfileAttribute from '../components/profile/ProfileAttribute';
 import Address from '../components/profile/Address';
+import getCustomerById from '../core/services/getCustomerById';
 
 function Profile() {
+  const isAuth = localStorage.getItem('isAuth');
+  const userID = localStorage.getItem('userID');
+  const [email, setEmail] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [dateOfBirth, setdateOfBirth] = useState('');
+  const [userVersion, setUserVersion] = useState(0);
+  if (isAuth === 'true' && userID) {
+    getCustomerById(userID).then(
+      (result) => {
+        setEmail(result.email);
+        setFirstName(result.firstName);
+        setLastName(result.lastName);
+        setdateOfBirth(result.dateOfBirth);
+        setUserVersion(result.version);
+      },
+      () => new Error(`Couldn't receive user information!`),
+    );
+  }
   return (
     <div>
       <Container>
         <header>Your profile</header>
-        <ProfileAttribute headerText='E-Mail' />
-        <ProfileAttribute headerText='First name' />
-        <ProfileAttribute headerText='Last name' />
-        <ProfileAttribute headerText='Date of birth' />
+        <ProfileAttribute
+          headerText='E-Mail'
+          value={email}
+          userID={userID}
+          userVersion={userVersion}
+        />
+        <ProfileAttribute
+          headerText='First name'
+          value={firstName}
+          userID={userID}
+          userVersion={userVersion}
+        />
+        <ProfileAttribute
+          headerText='Last name'
+          value={lastName}
+          userID={userID}
+          userVersion={userVersion}
+        />
+        <ProfileAttribute
+          headerText='Date of birth'
+          value={dateOfBirth}
+          type='date'
+          userID={userID}
+          userVersion={userVersion}
+        />
         <div>
           <header>Shipping addresses</header>
           <Address />
