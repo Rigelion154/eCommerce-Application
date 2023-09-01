00b15e2550e88857dc1a6dfedd74a468b90a34bd
v154254 <v154254@yandex.ru>
2023-09-02 01:57:25 +0500

    feat: add validation for editing address

--- src/components/profile/Address.tsx
+++ src/components/profile/Address.tsx
@@ -8,16 +8,25 @@ function AddressComponent({ ...props }: AddressWithID) {
   const [isNotDefault, setIsDefault] = useState(true);
   const [isNotShipping, setIsNotShipping] = useState(true);
   const [isNotBilling, setIsNotBilling] = useState(true);
-  const [newDefaultAddress, setNewDefaultAddress] = useState(false);
-  const [userVersion, setUserVersion] = useState(0);
+
   const [inputIsDisabled, changeInputDisabled] = useState(true);
   const [updateIsDisabled, changeUpdateDisabled] = useState(false);
   const [saveIsDisabled, changeSaveDisabled] = useState(true);
-  const [updateSuccess, toggleUpdateSuccess] = useState('');
+
+  const [streetError, setStreetError] = useState('');
+  const [cityError, setCityError] = useState('');
+  const [postalCodeError, setPostalCodeError] = useState('');
+
   const [streetName, setStreetName] = useState(props.streetName);
   const [city, setCity] = useState(props.city);
   const [postalCode, setPostalCode] = useState(props.postalCode);
   const [country, setCountry] = useState(props.country);
+
+  const [postalCodeValue, setPostalCodeValue] = useState(props.postalCode);
+
+  const [newDefaultAddress, setNewDefaultAddress] = useState(false);
+  const [userVersion, setUserVersion] = useState(0);
+  const [updateSuccess, toggleUpdateSuccess] = useState('');
 
   const userID = localStorage.getItem('userID') as string;
 
@@ -41,12 +50,70 @@ function AddressComponent({ ...props }: AddressWithID) {
       () => {},
     );
   }
+
   getUserInfo();
+
   function enableInput() {
     changeInputDisabled(false);
     changeUpdateDisabled(true);
     changeSaveDisabled(false);
   }
+
+  function checkCity(value: string) {
+    changeSaveDisabled(false);
+    setCityError('');
+    const validRegex = /^[a-zA-Zа-яА-Я]*$/;
+    if (!value) {
+      setCityError('This field is required');
+      changeSaveDisabled(true);
+    } else if (!value.match(validRegex)) {
+      setCityError('The field must not have any special characters or numbers');
+      changeSaveDisabled(true);
+    }
+  }
+
+  function checkStreet(value: string) {
+    changeSaveDisabled(false);
+    setStreetError('');
+    const validRegex = /^[a-zA-Zа-яА-Я]*$/;
+    if (!value) {
+      setStreetError('This field is required');
+      changeSaveDisabled(true);
+    } else if (!value.match(validRegex)) {
+      setStreetError('The field must not have any special characters or numbers');
+      changeSaveDisabled(true);
+    }
+  }
+
+  function checkPostalCode(value: string) {
+    let postalCodePattern: RegExp = /^$/;
+    setPostalCodeError('');
+    changeSaveDisabled(false);
+    if (!value) {
+      setPostalCodeError('This field is required');
+      changeSaveDisabled(true);
+    } else {
+      if (country === 'BY' || country === 'RU') {
+        postalCodePattern = /^\d{6}$/;
+      } else if (country === 'US') {
+        postalCodePattern = /^\d{5}$|^\d{5}-\d{4}$/;
+      } else if (country === 'CA') {
+        postalCodePattern = /^[A-Za-z]\d[A-Za-z] \d[A-Za-z]\d$/;
+      }
+      if (!postalCodePattern.test(value)) {
+        setPostalCodeError('Postal code does not match the format');
+        changeSaveDisabled(true);
+      }
+    }
+  }
+
+  function changeCountry(value: string) {
+    setCountry(value);
+    setPostalCodeValue('');
+    setPostalCodeError('This field is required');
+    changeSaveDisabled(true);
+  }
+
   function tryToUpdate() {
     const actions: Actions = [];
     actions.push({
@@ -86,6 +153,7 @@ function AddressComponent({ ...props }: AddressWithID) {
     changeUpdateDisabled(false);
     changeSaveDisabled(true);
   }
+
   return (
     <div>
       <p hidden={isNotDefault}>This is default address</p>
@@ -95,8 +163,10 @@ function AddressComponent({ ...props }: AddressWithID) {
         disabled={inputIsDisabled}
         onChange={(e) => {
           setCity(e.target.value);
+          checkCity(e.target.value);
         }}
       />
+      <span>{cityError}</span>
       <p />
       <span>Street:</span>
       <input
@@ -104,24 +174,30 @@ function AddressComponent({ ...props }: AddressWithID) {
         disabled={inputIsDisabled}
         onChange={(e) => {
           setStreetName(e.target.value);
+          checkStreet(e.target.value);
         }}
       />
+      <span>{streetError}</span>
       <p />
       <span>Postal code:</span>
       <input
         defaultValue={props.postalCode}
         disabled={inputIsDisabled}
+        value={postalCodeValue}
         onChange={(e) => {
           setPostalCode(e.target.value);
+          setPostalCodeValue(e.target.value);
+          checkPostalCode(e.target.value);
         }}
       />
+      <span>{postalCodeError}</span>
       <p />
       <span> Country:</span>
       <select
         defaultValue={props.country}
         disabled={inputIsDisabled}
         onChange={(e) => {
-          setCountry(e.target.value);
+          changeCountry(e.target.value);
         }}
       >
         <option value='BY'>Belarus</option>
@@ -131,11 +207,11 @@ function AddressComponent({ ...props }: AddressWithID) {
       </select>
       <div hidden={inputIsDisabled}>
         <label hidden={isNotShipping}>
-          Set as new shipping address
+          Set as new default shipping address
           <input type='checkbox' onChange={(e) => setNewDefaultAddress(e.target.checked)} />
         </label>
         <label hidden={isNotBilling}>
-          Set as new billing address
+          Set as new default billing address
           <input type='checkbox' onChange={(e) => setNewDefaultAddress(e.target.checked)} />
         </label>
       </div>
